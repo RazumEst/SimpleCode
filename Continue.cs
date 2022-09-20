f8e4efab02ba7f4515d8bf3e9ae9f931109fc8d2
RazumEst <109899643+RazumEst@users.noreply.github.com>
2022-09-20 10:58:08 +0400

    Add files via upload

---
+++ Continue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Continue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 0; a < 40; a++)
+            {
+                if (a % 2 == 0)
+                {
+                    continue; // оператор пропуска, но код будет продолжаться дальше
+                }
+                Console.WriteLine(a);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ключевое слово break. С его помощью можно прервать выполнение цикла.
+            //for (int a = 0; a < 100; a++)
+            //{
+            //    Console.WriteLine(a);
+
+            //    if (a  == 40)
+            //    {
+            //        break;
+            //    }
+            //}
+
+            for (int r = 0; r < 10; r++)
+            {
+                Console.WriteLine("Нажмите любую клавишу, кроме w, чтобы продолжить выполнение программы");
+                string msg = Console.ReadLine();
+                if (msg == "w")
+                {
+                    break;
+                }
+                Console.WriteLine(r);
+
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
